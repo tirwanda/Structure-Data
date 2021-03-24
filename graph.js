481d5a4ca5ed601c15c8f396711b8f881502d8d5
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-03-24 12:56:36 +0700

    Graph

---
+++ graph.js
@@ -0,0 +1,81 @@
+function createNode(key) {
+	const neighbors = [];
+
+	return {
+		key,
+		neighbors,
+
+		addNeighbor: function (node) {
+			neighbors.push(node);
+		},
+	};
+}
+
+function createGraph(directed = false) {
+	const nodes = [];
+	const edges = [];
+
+	return {
+		directed,
+		nodes,
+		edges,
+
+		addNode: function (key) {
+			const newNode = createNode(key);
+			nodes.push(newNode);
+		},
+
+		getNode: function (key) {
+			return nodes.find(function (node) {
+				return node.key === key;
+			});
+		},
+
+		addEdges: function (node1key, node2key) {
+			const node1 = this.getNode(node1key);
+			const node2 = this.getNode(node2key);
+
+			node1.addNeighbor(node2);
+			if (!directed) {
+				node2.addNeighbor(node1);
+			}
+		},
+
+		print: function () {
+			return nodes
+				.map(function ({ key, neighbors }) {
+					let result = key;
+
+					if (neighbors.length) {
+						result += `=> ${neighbors
+							.map(function (neighbor) {
+								return neighbor.key;
+							})
+							.join(' ')}`;
+					}
+
+					return result;
+				})
+				.join('\n');
+		},
+	};
+}
+
+const graph = createGraph(true);
+
+graph.addNode('Ani');
+graph.addNode('Banu');
+graph.addNode('Cindy');
+graph.addNode('Dina');
+graph.addNode('Elisa');
+graph.addNode('Fadli');
+
+graph.addEdges('Ani', 'Banu');
+graph.addEdges('Banu', 'Cindy');
+graph.addEdges('Banu', 'Dina');
+graph.addEdges('Ani', 'Dina');
+graph.addEdges('Dina', 'Fadli');
+graph.addEdges('Elisa', 'Fadli');
+graph.addEdges('Fadli', 'Ani');
+
+console.log(graph.print());
